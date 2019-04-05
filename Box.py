0bd1dd111ff7f352238ebc24f518f43a18d5fa6a
HaykTheKhulyan <hkhulyan@gmail.com>
2019-04-04 23:45:35 -0700

    Changed width and height member variables to private

--- Box.py
+++ Box.py
@@ -5,8 +5,8 @@ class Box:
     def __init__(self, x_pos, y_pos, width, height, fill_color=colors.BLACK, border_color=colors.BLACK, border_thickness=0):
         self._x_pos = x_pos
         self._y_pos = y_pos
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
 
         self.fill_color = fill_color
         self.border_color = border_color
@@ -19,12 +19,12 @@ class Box:
                              self.border_color,                                             # the color of the border rect
                             (self._x_pos - self.border_thickness,                           # the x position of the top left corner of the border rect
                              self._y_pos - self.border_thickness,                           # the y position of the top left corner of the border rect
-                             self.width + (2 * self.border_thickness),                      # the width of the border rect
-                             self.height + (2 * self.border_thickness)))                    # the height of the border rect
+                             self._width + (2 * self.border_thickness),                     # the width of the border rect
+                             self._height + (2 * self.border_thickness)))                   # the height of the border rect
 
         pygame.draw.rect(screen,                    # the surface upon which to draw on
                          self.fill_color,           # the color of the rect
                         (self._x_pos,               # the x position of the top left corner of the rect
                          self._y_pos,               # the y position of the top left corner of the rect
-                         self.width,                # the width of the rect
-                         self.height))              # the height of the rect
+                         self._width,               # the width of the rect
+                         self._height))             # the height of the rect
